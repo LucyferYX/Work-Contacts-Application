de234fbae9de2db6549ca74c962e742fd040e9f6
Liene <liene.krista@gmail.com>
2023-08-24 01:18:22 +0300

    Search function, clear button

--- Work Contacts Application/Work Contacts Application/Controllers/MainViewController.swift
+++ Work Contacts Application/Work Contacts Application/Controllers/MainViewController.swift
@@ -7,17 +7,19 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UITableViewDataSource {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var employeeTableView: UITableView!
+    @IBOutlet weak var cancelButton: UIButton!
     
     var tallinURL = "https://tallinn-jobapp.aw.ee/employee_list/"
     var tartuURL = "https://tartu-jobapp.aw.ee/employee_list/"
     
     // Dictionary, employees are sorted by position
     var employeesByPosition = [Position: [Employee]]()
+    var allEmployees: [Employee] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +29,33 @@ class MainViewController: UIViewController, UITableViewDataSource {
         
         searchBar.text = ""
         searchBar.backgroundImage = UIImage()
+        searchBar.delegate = self
         
+        cancelButton.isHidden = true
+        
+        employeeTableView.delegate = self
         employeeTableView.dataSource = self
+        
         fetchEmployees()
     }
     
     
     // Fethcing all the employees to display
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        employeesByPosition = [:]
+        for employee in allEmployees {
+            if searchText.isEmpty ||
+                employee.fname.lowercased().contains(searchText.lowercased()) ||
+                employee.lname.lowercased().contains(searchText.lowercased()) ||
+                employee.contactDetails.email.lowercased().contains(searchText.lowercased()) ||
+                employee.position.rawValue.lowercased().contains(searchText.lowercased()) ||
+                (employee.projects?.contains(where: { $0.lowercased().contains(searchText.lowercased()) }) ?? false) {
+                    self.employeesByPosition[employee.position, default: []].append(employee)
+            }
+        }
+        employeeTableView.reloadData()
+    }
+
     func fetchEmployees() {
         let urls = [
             URL(string: tallinURL)!,
@@ -45,6 +67,7 @@ class MainViewController: UIViewController, UITableViewDataSource {
                 guard let data = data else { return }
                 do {
                     let employeeList = try JSONDecoder().decode(EmployeeList.self, from: data)
+                    self.allEmployees.append(contentsOf: employeeList.employees)
                     for employee in employeeList.employees {
                         self.employeesByPosition[employee.position, default: []].append(employee)
                     }
@@ -57,8 +80,8 @@ class MainViewController: UIViewController, UITableViewDataSource {
             }.resume()
         }
     }
+
     
-    // MARK: - UITableViewDataSource
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return Position.allCases.count
@@ -98,11 +121,52 @@ class MainViewController: UIViewController, UITableViewDataSource {
         }
         return cell
     }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        UIView.animate(withDuration: 0.3) {
+            self.cancelButton.isHidden = false
+        }
+    }
+
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            self.cancelButton.isHidden = true
+            self.searchBar.text = ""
+            self.searchBar.resignFirstResponder()
+        }
+        // Cancel button refreshes the table view
+        searchBar(searchBar, textDidChange: "")
+    }
+
 
 }
 
 
-// Employees
+extension MainViewController {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .systemGray6
+        
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.text = self.tableView(tableView, titleForHeaderInSection: section)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        // Padding
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 5),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -5),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+        ])
+        
+        return view
+    }
+
+}
+
+
+
 struct Employee: Codable {
     let fname: String
     let lname: String
